1cf70e866f229b4ddd9c7e38ad69bacefa5b5357
anthony-shaw <anthony.p.shaw@gmail.com>
2016-01-27 14:26:49 +1100

    white image

--- gitcloud/__main__.py
+++ gitcloud/__main__.py
@@ -7,7 +7,7 @@ contributors = response.json()
 weight=[]
 for contributor in contributors:
     weight.append(('@' + contributor['login'], int(contributor['contributions'])))
-tags = make_tags(weight[:50], minsize=50, maxsize=100)
-create_tag_image(tags, 'tags.png', size=(1500,1500), background=(0, 0, 0, 255), layout=LAYOUT_MIX, fontname='Molengo', rectangular=True)
+tags = make_tags(weight[:100], minsize=50, maxsize=230)
+create_tag_image(tags, 'tags.png', size=(1500,1500), layout=LAYOUT_MIX, fontname='Molengo', rectangular=True)
 import webbrowser
 webbrowser.open('tags.png') # see results
