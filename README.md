5aaaf9d9239ebae66c9945ff2d2ba27fb73324bd
anthony-shaw <anthony.p.shaw@gmail.com>
2016-01-27 14:13:30 +1100

    Initial concept and design

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # github-contrib-tag-cloud
-Create a tag cloud from a contributor history
+Create a tag cloud from a contributor history on GitHub
+
+
+# Installing pre-requisites
+Download and install pygame http://www.pygame.org/download.shtml
+
+```bash
+pip install -r requirements.txt
+```
---
+++ gitcloud/__main__.py
@@ -0,0 +1,13 @@
+from pytagcloud import create_tag_image, make_tags, LAYOUT_MIX
+import simplejson as json
+import requests
+
+response = requests.get("https://api.github.com/repos/apache/libcloud/contributors")
+contributors = response.json()
+weight=[]
+for contributor in contributors:
+    weight.append(('@' + contributor['login'], int(contributor['contributions'])))
+tags = make_tags(weight[:50], minsize=50, maxsize=100)
+create_tag_image(tags, 'tags.png', size=(1500,1500), background=(0, 0, 0, 255), layout=LAYOUT_MIX, fontname='Molengo', rectangular=True)
+import webbrowser
+webbrowser.open('tags.png') # see results
